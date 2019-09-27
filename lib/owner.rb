cc8ff8f802e327b0b04770262aa85614d6dc8f24
Sarah H <sarahrose.henderson@gmail.com>
2019-09-27 01:18:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,7 @@ class Owner
 
     def list_pets
       @pets.each do |pet_type, pet_array|
-        pet_array.each.count do |pet|
+        pet_array.each.size do |pet|
            "I have #{pet.dogs} dog(s), and #{pet.cats} cat(s)."
          end
      end
